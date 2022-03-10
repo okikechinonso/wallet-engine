e9693a2af4b8b7e292c92d55d724fc8445e815f2
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-10 20:22:57 +0100

    created deposite handler

--- infrastructure/persistence/mysql/user_db.go
+++ infrastructure/persistence/mysql/user_db.go
@@ -1,33 +1,54 @@
 package mysql
 
-import "wallet-engine/domain/entity"
+import (
+	"wallet-engine/domain/entity"
+)
 
-func NewUser(user entity.User) error  {
+func (d *Database) NewUser(user entity.User) (*entity.User, error) {
+	result := d.PgDB.Create(&user)
+	return &user, result.Error
+}
+
+func (d *Database) NewWallet(walletAddress, userID string) (*entity.Wallet, error) {
+	wallet := entity.Wallet{}
+	wallet.WalletAddress = walletAddress
+	wallet.UserID = userID
+	result := d.PgDB.Create(&wallet)
+	return &wallet, result.Error
+}
+
+func (d *Database) FindWallet(walletID string) (*entity.Wallet, error) {
+	var wallet entity.Wallet
+	err := d.PgDB.Where("wallet_address= ?", walletID).First(&wallet).Error
+	return &wallet, err
+}
+func (d *Database) CreditdWallet(amount int64) error {
 	return nil
 }
-
-func (d *Database) NewWallet(user entity.User) (*entity.User,error) {
-	result := d.PgDB.Create(user)
-	return nil,result.Error
-}
-func (d *Database) FindWallet(walletID string) (*entity.User, error) {
-	var user entity.W
-	err := d.PgDB.Where("email = ?", walletID).First(&user).Error
+func (d *Database) FindUserByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	err := d.PgDB.Where("email = ?", email).First(&user).Error
 	return &user, err
 }
-func ( d *Database) CreditdWallet(amount int64) error {
-	return nil
-}
-func (d *Database) FindUserByEmail(email string) (*entity.User, error){
-	return nil, nil
+
+func (d *Database) TokenInBlacklist(*string) bool {
+	return false
 }
 
-func (d *Database) TokenInBlacklist(*string) bool{
-return false
-}
-
-func (d *Database) DebitWallet(amount int64) error{
+func (d *Database) DebitWallet(amount int64) error {
 	return nil
 }
 
+func (d *Database) GetPhone(userphone string) (string, error) {
+	return "", nil
+}
 
+func (d *Database) CreateTransaction(transaction *entity.Transaction) error {
+	result := d.PgDB.Create(transaction)
+	return result.Error
+}
+
+func (d *Database) UpdateWallet(wallet entity.Wallet) error {
+	result := d.PgDB.Model(&wallet).Where("wallet_address = ?", wallet.WalletAddress).Update("balance", wallet.Balance)
+	return result.Error
+}
