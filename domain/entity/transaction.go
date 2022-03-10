c750c79175f434b378d2c594a83480d9447da4e3
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-10 14:31:11 +0100

    added userid field

--- domain/entity/transaction.go
+++ domain/entity/transaction.go
@@ -6,6 +6,7 @@ type Transaction struct {
 	Reference              string    `json:"reference"`
 	SenderWalletAddress    string    `json:"sender_wallet_address"`
 	RecipientWalletAddress string    `json:"Recipient_walllet_address"`
+	UserID                 string    `json:"user_id" gorm:"foreignkey:User(id)"`
 	Phone                  string    `json:"phone"`
 	Amount                 string    `json:"amount"`
 	Currency               string    `json:"currency"`
